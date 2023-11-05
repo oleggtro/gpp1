6cace8ea968bd254a83a0abbf50200c4ddd81d36
Ole <ole@oleggtro.com>
2023-11-05 18:08:11 +0100

    Erstes Programm MyName

---
+++ GdP1/Code/MyName/myname.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+
+int main() {
+char name[] = "ole";
+int day = 9;
+int month = 1;
+int year = 2005;
+
+printf("Mein Name ist %s\n",name);
+printf("Mein Geburtsdatum ist der %02d.%02d.%04d\n",day,month,year);
+}
+
